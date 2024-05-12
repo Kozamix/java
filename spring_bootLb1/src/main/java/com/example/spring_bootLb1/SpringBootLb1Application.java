db64ee2e5c717e3501764dd90249650481d984d2
Kozamix <misha244king@gmail.com>
2024-05-12 23:24:55 +0300

    створення проєкту

---
+++ spring_bootLb1/src/main/java/com/example/spring_bootLb1/SpringBootLb1Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_bootLb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLb1Application.class, args);
+	}
+
+}
---
+++ spring_bootLb1/src/test/java/com/example/spring_bootLb1/SpringBootLb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_bootLb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
